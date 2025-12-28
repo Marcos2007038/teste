9633585686a2c994db9396c33645a4f8cdb5b6a8
Antônio Marcos <163885518+Marcos2007038@users.noreply.github.com>
2025-12-28 18:57:11 -0300

    fdf
    fd

---
+++ TesteRender/src/main/java/TesteRender/TesteRender/TesteRenderApplication.java
@@ -0,0 +1,13 @@
+package TesteRender.TesteRender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteRenderApplication.class, args);
+	}
+
+}
---
+++ TesteRender/src/test/java/TesteRender/TesteRender/TesteRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package TesteRender.TesteRender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
